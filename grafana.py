223042e4d6d700cafc79d2cf66a23f48c009b039
Mateusz-Szymczyk <Mateusz.Szymczyk@shell.com>
2021-06-14 20:23:39 +0200

    getUsers()

--- grafana.py
+++ grafana.py
@@ -3,15 +3,27 @@ import http.client
 import json
 
 conn = http.client.HTTPSConnection("monitoring.ops.sdu-rds.com")
+
 payload = ''
 headers = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
   'Authorization': 'Bearer eyJrIjoiMFpuTklEU2RabjZNR3ZMYWh0aWNiNDZMNHBOQWZXVWEiLCJuIjoibW9kaWZ5QWNjZXNzIiwiaWQiOjF9'
 }
-conn.request("GET", "/api/folders", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
 
-##
+def getFolders():
+  print("Hello from a function")
+  conn.request("GET", "/api/folders", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  print(data.decode("utf-8"))
+
+def getUsers():
+  conn.request("GET", "/api/org/users", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  print(data.decode("utf-8"))
+
+
+getFolders()
+# getUsers()
