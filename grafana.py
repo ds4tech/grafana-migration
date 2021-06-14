e3e7e929fa24e1b3d44ac77d424921221a11d96d
ds4tech <ds4tech@gmail.com>
2021-06-14 14:15:05 +0200

    cURL

--- grafana.py
+++ grafana.py
@@ -1,1 +1,15 @@
-print ("hello")
+
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("monitoring.ops.sdu-rds.com")
+payload = ''
+headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Bearer eyJrIjoiMFpuTklEU2RabjZNR3ZMYWh0aWNiNDZMNHBOQWZXVWEiLCJuIjoibW9kaWZ5QWNjZXNzIiwiaWQiOjF9'
+}
+conn.request("GET", "/api/folders", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
